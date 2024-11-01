e2cc75fa3cbee85b6807d9d0bbd0e19a0b57e546
Erskine Gamaliel L. Duenas <erskine.duenas@gmail.com>
2024-11-01 15:28:35 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+/** references/notes
+ * asciitable.com
+ * base64 index table
+ * Location: Domain --> Path --> Query String --> Hash/Fragment
+ *
+ * print 'window.location' function in browser to find location
+ */
+
+/**
+ * Learned the command "npx now" to easily deploy
+ */
+
+const { hash } = window.location;
+message = atob(hash.replace("#", ""));
+
+if (message) {
+  document.querySelector("#message-form").classList.add("hide");
+  document.querySelector("#message-show").classList.remove("hide");
+  document.querySelector("h1").innerHTML = message;
+}
+document.querySelector("form").addEventListener("submit", (event) => {
+  //stop the default browser behaviour upon submission
+  //default behaviour upon submission: send the value to back-end server
+  event.preventDefault();
+
+  document.querySelector("#message-form").classList.add("hide");
+  document.querySelector("#link-form").classList.remove("hide");
+
+  const input = document.querySelector("#message-input");
+  //btoa = binary to ascii
+  const encrypted = btoa(input.value);
+
+  const linkInput = document.querySelector("#link-input");
+
+  linkInput.value = `${window.location}#${encrypted}`;
+  linkInput.select();
+});
